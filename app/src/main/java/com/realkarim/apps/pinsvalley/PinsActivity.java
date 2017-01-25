8e5fb1910aae345c94d4875a09eca962a180cf50
Karim Mostafa <karim.mostafa.18@gmail.com>
2017-01-25 16:11:53 +0200

    First commit

---
+++ app/src/main/java/com/realkarim/apps/pinsvalley/PinsActivity.java
@@ -0,0 +1,13 @@
+package com.realkarim.apps.pinsvalley;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PinsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pins);
+    }
+}
